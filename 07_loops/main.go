e492810f1f081677a00692305cbda3c392d7e748
Muttakin Islam Hasib <muttakinislamhasib@gmail.com>
2026-09-11 23:30:08 +0600

    Loops

---
+++ 07_loops/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Long method
+	i := 1
+
+	for i <= 10 {
+		fmt.Println(i)
+		i++
+	}
+
+	// Short method
+	for i := 1; i <= 10; i++ {
+		fmt.Printf("Number is %d", i)
+	}
+}
